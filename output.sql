16427aca8ceb163da694ebc9a19be3f9e6e50c52
Patricio Labin Correa <plabin@outlook.cl>
2017-06-19 14:52:42 -0400

    Delete output.sql

--- output.sql
+++
@@ -1,9 +0,0 @@
-/* Generated with Xl_to_SQL.py -- Patricio Labin Correa
-====================================================================*/
-insert into example_table_transactions values(1,1,1,10,100,"2016-01-21 00:00:00");
-insert into example_table_transactions values(2,1,2,2,200,"2017-02-20 00:00:00");
-insert into example_table_transactions values(3,2,3,1,100,"2015-08-21 00:00:00");
-insert into example_table_transactions values(4,2,4,3,200,"2015-05-22 00:00:00");
-insert into example_table_transactions values(5,3,5,4,100,"2014-11-10 00:00:00");
-insert into example_table_transactions values(6,3,1,5,300,"2018-12-12 00:00:00");
-
